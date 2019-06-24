5d81c83a9d08f9be85cc7195e7ffcf65c2369983
huangdingluan <316943766@qq.com>
2019-06-24 23:27:16 +0800

    2

--- src/新特性/apple.java
+++ src/新特性/apple.java
@@ -1,18 +1,46 @@
 package ÐÂÌØÐÔ;
 
+import java.util.function.Consumer;
+
 public class apple {
-	private String color;
-	private float weight;
+	private static String color;
+	private static float weight;
 	
 	public apple(String color, float weight) {
 		super();
 		this.color = color;
 		this.weight = weight;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public apple() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public static void main(String[] args) {
+		System.out.println("apple:"+color+"/"+weight);
+	}
+
+
+
+
+	public static void ConsumerApple(apple[] apps,Consumer<apple> c){
+		 for(apple app:apps){
+			 c.accept(app);
+		 }
+	 }
 
 }
