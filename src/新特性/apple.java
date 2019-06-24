0f42eda1bff2231766c92fab5d30e983574bb8d0
huangdingluan <316943766@qq.com>
2019-06-24 23:06:06 +0800

    2

--- src/新特性/apple.java
+++ src/新特性/apple.java
@@ -1,6 +1,14 @@
 package ÐÂÌØÐÔ;
 
 public class apple {
+	private String color;
+	private float weight;
+	
+	public apple(String color, float weight) {
+		super();
+		this.color = color;
+		this.weight = weight;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
